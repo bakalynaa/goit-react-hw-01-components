b121e9ae296540dbcd296295c784a55418394279
bakalynaa <137291201+StudradaFITM@users.noreply.github.com>
2023-09-07 22:11:19 +0300

    deleted mistakes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,8 +27,8 @@ export const App = () => {
       <Profile user={user} />
       <Statistics title='Upload stats' stats={data} />
       <Statistics stats={data} />
-      <FriendList friends={friends} />;
-      <TransactionHistory items={transactions} />;
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
